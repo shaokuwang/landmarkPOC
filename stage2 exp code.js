a25c1c3309639a1ebb020d59e912223f2a6c742e
ku.wang[王少谷] <ku.wang@104.com.tw>
2020-06-11 17:54:34 +0800

    new function : impressionOnly

--- stage2 exp code.js
+++ stage2 exp code.js
@@ -47,6 +47,17 @@
     });     
   }
   
+  function impressionOnly(group){
+    landmark_changelink[0].setAttribute('href' , landmark_newJobsource + group );
+    ga('send', {
+      hitType: 'event',
+      eventCategory: '地標POC',
+      eventAction: 'impression_Group'+ group,
+      eventLabel: jobNo
+    });     
+
+  }
+
   try{
   landmark_blacklist = [];
   landmarkPocClassifier = '{{JS - random_classifier}}'; //0,1,2
@@ -63,88 +74,87 @@
   }catch(e){}
   
   function landmark_show(){
-  try{
-
-  joblistItem = document.querySelectorAll('.job-list-item');
-  landmarkStart = 0;
-  landmarkEnd = joblistItem.length;
-  landmarkPocJobNoList_MRT = landmark_MRT_jobs;
-  landmarkPocJobNoList_other = landmark_Other_jobs;
-  landmarkPocClassifier = '{{JS - random_classifier}}'; //0,1,2
+    try{
+      joblistItem = document.querySelectorAll('.job-list-item');
+      landmarkStart = 0;
+      landmarkEnd = joblistItem.length;
+      landmarkPocJobNoList_MRT = landmark_MRT_jobs;
+      landmarkPocJobNoList_other = landmark_Other_jobs;
+      landmarkPocClassifier = '{{JS - random_classifier}}'; //0,1,2
   
-  for( updatejob_key in landmark_updateJob ){
-    landmarkPocJobNoList_MRT[ updatejob_key ] = landmark_updateJob[updatejob_key];
-  }
-  landmarkPocMRTList = {{地標POC測試_MRT列表}};
-  landmarkPocOtherList = {{地標POC測試_其他地標列表}}
-  ga('create', 'UA-165589325-1', 'auto');
+      for( updatejob_key in landmark_updateJob ){
+        landmarkPocJobNoList_MRT[ updatejob_key ] = landmark_updateJob[updatejob_key];
+      }
+      landmarkPocMRTList = {{地標POC測試_MRT列表}};
+      landmarkPocOtherList = {{地標POC測試_其他地標列表}}
+      ga('create', 'UA-165589325-1', 'auto');
   
-  if( landmark_loadtime * landmark_intervalms > 4000 ){
-    return;
-  }
+      if( landmark_loadtime * landmark_intervalms > 4000 ){
+        return;
+      }
     
-  for(i=landmarkStart; i<landmarkEnd;i+=1){
+      for(i=landmarkStart; i<landmarkEnd;i+=1){
 
-    var jobNo = joblistItem[i].dataset.jobNo;
+        var jobNo = joblistItem[i].dataset.jobNo;
 
-    if(jobNo){
-      if( typeof( landmarkPocJobNoList_MRT[jobNo] ) != 'undefined' ){
-        if( joblistItem[i].dataset.jobsource == 'hotjob_chr' || landmark_blacklist.indexOf(jobNo) != -1){
+        if(jobNo){
+          if( typeof( landmarkPocJobNoList_MRT[jobNo] ) != 'undefined' ){
+            if( joblistItem[i].dataset.jobsource == 'hotjob_chr' || landmark_blacklist.indexOf(jobNo) != -1){
+              continue;
+            }
+            var source_poc = "MRT";
+            nearStation = landmarkPocMRTList [ landmarkPocJobNoList_MRT[jobNo][1] ];
+            nearStationDist = landmarkPocJobNoList_MRT[jobNo][0];
+            landmark_changelink = joblistItem[i].querySelectorAll('.js-job-link');
+            landmark_newJobsource = landmark_changelink[0].getAttribute('href').split('jobsource=')[0] + 'jobsource=landmarkPOC_';
+          }else if( typeof( landmarkPocJobNoList_other[jobNo] ) != 'undefined'){
+            if( joblistItem[i].dataset.jobsource == 'hotjob_chr' || landmark_blacklist.indexOf(jobNo) != -1){
+              continue;
+            }
+            var source_poc = "Other";
+            nearStation = landmarkPocOtherList [ landmarkPocJobNoList_other[jobNo][1] ];
+            nearStationDist = landmarkPocJobNoList_other[jobNo][0];
+            landmark_changelink = joblistItem[i].querySelectorAll('.js-job-link');
+            landmark_newJobsource = landmark_changelink[0].getAttribute('href').split('jobsource=')[0] + 'jobsource=landmarkPOC_';
+          }else{
           continue;
         }
-        var source_poc = "MRT";
-        nearStation = landmarkPocMRTList [ landmarkPocJobNoList_MRT[jobNo][1] ];
-        nearStationDist = landmarkPocJobNoList_MRT[jobNo][0];
-        landmark_changelink = joblistItem[i].querySelectorAll('.js-job-link');
-        landmark_newJobsource = landmark_changelink[0].getAttribute('href').split('jobsource=')[0] + 'jobsource=landmarkPOC_';
-      }else if( typeof( landmarkPocJobNoList_other[jobNo] ) != 'undefined'){
-        if( joblistItem[i].dataset.jobsource == 'hotjob_chr' || landmark_blacklist.indexOf(jobNo) != -1){
+        }else{
           continue;
         }
-        var source_poc = "Other";
-        nearStation = landmarkPocOtherList [ landmarkPocJobNoList_other[jobNo][1] ];
-        nearStationDist = landmarkPocJobNoList_other[jobNo][0];
-        landmark_changelink = joblistItem[i].querySelectorAll('.js-job-link');
-        landmark_newJobsource = landmark_changelink[0].getAttribute('href').split('jobsource=')[0] + 'jobsource=landmarkPOC_';
-      }else{
-      continue;
-    }
-    }else{
-      continue;
-    }
 
-    switch (String(landmarkPocClassifier)+source_poc){
-      case "1MRT" :
-      replaceContent("AA");
-        break;
-      case "2MRT" :
-      replaceContent("B");        
-        break;
-      case "0MRT"  :
-      replaceContent("C");
-        break
-      case "1Other" :
-      replaceContent("D");
-        break;
-      case "2Other" :
-      replaceContent("E");
-        break;
-      case "0Other" :
-      replaceContent("F");
-        break;
-        default :
-        ga('send', {
-          hitType: 'event',
-          eventCategory: '地標POC',
-          eventAction: 'Error',
-          eventLabel: jobNo
-        });
-        break;
+        switch (String(landmarkPocClassifier)+source_poc){
+          case "1MRT" :
+          replaceContent("AA");
+            break;
+          case "2MRT" :
+          replaceContent("B");        
+            break;
+          case "0MRT"  :
+          impressionOnly("C");
+            break
+          case "1Other" :
+          replaceContent("D");
+            break;
+          case "2Other" :
+          impressionOnly("E");
+            break;
+          case "0Other" :
+          impressionOnly("F");
+            break;
+          default :
+          ga('send', {
+            hitType: 'event',
+            eventCategory: '地標POC',
+            eventAction: 'Error',
+            eventLabel: jobNo
+          });
+          break;
+        }
+      }
+    }catch(e){
+      console.log(e)
     }
-  }
-  }catch(e){
-    console.log(e)
-  }
   }
   
   var landmark_loadtime = 1;
